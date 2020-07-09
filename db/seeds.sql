d7a49b6e6c0d068bfe40165090c851dfa0c265db
Patrick Urbankowski <purbankowski@gmail.com>
2020-07-09 00:49:23 -0400

    added initial code to schema.sql seeds.sql connection.js and server.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Whonper Borger');
+INSERT INTO burgers (burger_name) VALUES ('Slim Jim Burger');
+INSERT INTO burgers (burger_name) VALUES ('Icy Hot Burger');
